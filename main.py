d04c742053710912eba5c14c9358d4f6c3e6bf6d
danivdwerf <danivdwerf@gmail.com>
2023-03-31 14:37:50 +0200

    Setup logger and settings

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import os
-import decky_plugin
+import logging
+from settings import SettingsManager
+
+# Set up logger
+logging.basicConfig(filename=os.path.join(os.environ["DECKY_PLUGIN_LOG_DIR"], 'backend.log'), format='[free-game-checker] %(asctime)s %(levelname)s %(message)s', filemode='w+', force=True)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Set up settings
+settings = SettingsManager(name="settings", settings_directory=os.environ["DECKY_PLUGIN_SETTINGS_DIR"])
+settings.read()
 
 class Plugin:
     async def _main(self):
-        decky_plugin.logger.info("free-game-checker loading")
+        logger.info("loading...")
         pass
 
     async def _unload(self):
-        decky_plugin.logger.info("free-game-checker unloading")
+        logger.info("unloading...")
         pass
 
+    async def getSetting(self, key: str, defaults):
+        logger.info("Get setting: '{}'".format(key))
+        return settings.getSetting(key, defaults)
+
+    async def setSetting(self, key: str, value):
+        loggin.info("Set setting: '{}': {}").format(key, value)
+    
     async def log(self, message: str):
-        decky_plugin.logger.info(message)
+        logger.info(message)
