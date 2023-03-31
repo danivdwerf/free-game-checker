6966bad23b11772406940e78dbe822d09264f322
danivdwerf <danivdwerf@gmail.com>
2023-03-31 17:19:38 +0200

    Added showToast function to backend class

--- src/lib/Backend.ts
+++ src/lib/Backend.ts
@@ -25,4 +25,24 @@ export default class Backend
         const output = await this._server?.callPluginMethod(name, args);
         return output.result;
     }
+
+    /**
+     * Show toast with sound.
+     * 
+     * @param title 
+     * @param message 
+     * @param sound 
+     * @param duration 
+     */
+    public static async showToast(title: string, message: string, sound: number = 6, duration?: number)
+    {
+        Backend.server.toaster.toast({
+            title: title,
+            sound: sound,
+            body: message,
+            playSound: true,
+            showToast: true,
+            duration: duration,
+        });
+    }
 };
